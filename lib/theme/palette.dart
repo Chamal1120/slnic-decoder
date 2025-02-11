e292a6d74011d08c24d976715b7ac7bac88c391f
Chamal1120 <kenway213beep@gmail.com>
2025-02-12 00:13:37 +0530

    feat: add my own custom catppuccin color pallete

---
+++ lib/theme/palette.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class CatppuccinMocha {
+  final Color rosewater;
+  final Color flamingo;
+  final Color pink;
+  final Color mauve;
+  final Color red;
+  final Color maroon;
+  final Color peach;
+  final Color yellow;
+  final Color green;
+  final Color teal;
+  final Color sky;
+  final Color sapphire;
+  final Color blue;
+  final Color lavender;
+  final Color text;
+  final Color subtext1;
+  final Color subtext0;
+  final Color overlay2;
+  final Color overlay1;
+  final Color overlay0;
+  final Color surface2;
+  final Color surface1;
+  final Color surface0;
+  final Color base;
+  final Color mantle;
+  final Color crust;
+
+  const CatppuccinMocha({
+    required this.rosewater,
+    required this.flamingo,
+    required this.pink,
+    required this.mauve,
+    required this.red,
+    required this.maroon,
+    required this.peach,
+    required this.yellow,
+    required this.green,
+    required this.teal,
+    required this.sky,
+    required this.sapphire,
+    required this.blue,
+    required this.lavender,
+    required this.text,
+    required this.subtext1,
+    required this.subtext0,
+    required this.overlay2,
+    required this.overlay1,
+    required this.overlay0,
+    required this.surface2,
+    required this.surface1,
+    required this.surface0,
+    required this.base,
+    required this.mantle,
+    required this.crust,
+  });
+
+  // Catppuccin Mocha Theme Colors
+  static const CatppuccinMocha flavor = CatppuccinMocha(
+    rosewater: Color(0xFFF5E0DC),
+    flamingo: Color(0xFFF2CDCD),
+    pink: Color(0xFFF5C2E7),
+    mauve: Color(0xFFCBA6F7),
+    red: Color(0xFFF38BA8),
+    maroon: Color(0xFFEBA0AC),
+    peach: Color(0xFFFAB387),
+    yellow: Color(0xFFF9E2AF),
+    green: Color(0xFFA6E3A1),
+    teal: Color(0xFF94E2D5),
+    sky: Color(0xFF89DCEB),
+    sapphire: Color(0xFF74C7EC),
+    blue: Color(0xFF89B4FA),
+    lavender: Color(0xFFB4BEFE),
+    text: Color(0xFFCDD6F4),
+    subtext1: Color(0xFFBAC2DE),
+    subtext0: Color(0xFFA6ADC8),
+    overlay2: Color(0xFF9399B2),
+    overlay1: Color(0xFF7F849C),
+    overlay0: Color(0xFF6C7086),
+    surface2: Color(0xFF585B70),
+    surface1: Color(0xFF45475A),
+    surface0: Color(0xFF313244),
+    base: Color(0xFF1E1E2E),
+    mantle: Color(0xFF181825),
+    crust: Color(0xFF11111B),
+  );
+}
+
